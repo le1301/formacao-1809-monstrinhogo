1f92a3c71c4ee69a59ab8aed58beb580779bb25e
le1301 <50032568+le1301@users.noreply.github.com>
2024-09-18 10:19:45 -0300

    pronto

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
 let steps = 0
 basic.forever(function () {
-    if (input.acceleration(Dimension.Strength) > 0) {
-    	
-    } else {
-    	
+    if (input.acceleration(Dimension.Strength) > 1500) {
+        steps += 1
+        basic.showNumber(steps)
+    } else if (steps == 15) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            # # . # #
+            . # # # .
+            `)
+        basic.pause(1000)
+        music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+        basic.showIcon(IconNames.Duck)
+        basic.pause(5000)
+        basic.clearScreen()
+        steps = 0
     }
 })
